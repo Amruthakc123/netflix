d8e47e839fde9dd1d898008b5baa6e1a29070724
amrutha <kcamrutha162@gmail.com>
2021-12-10 23:19:28 +0530

    this is netflix

---
+++ src/Section5/Section5.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Section5.css"
+
+
+const Section5 = () => {
+    return (
+        <div id="sec5">
+            <div id="sec5left">
+                <img src="https://occ-0-1946-2186.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABVxdX2WnFSp49eXb1do0euaj-F8upNImjofE77XStKhf5kUHG94DPlTiGYqPeYNtiox-82NWEK0Ls3CnLe3WWClGdiJP.png?r=5cf" alt="mobile"/>
+
+            </div>
+            <div id="sec5right">
+                <h1>Download your shows to watch offline.</h1>
+                <h3>Save your favourites easily and always have something to watch.</h3>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Section5
